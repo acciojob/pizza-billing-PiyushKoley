a3ecd4de140fcff5284f243cf54ce3fd998f9b1c
piyushkoley <piyushkoley@gmail.com>
2022-10-12 17:47:56 +0530

    made all changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,25 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    private final Boolean isVeg;
     private String bill;
-
+    private boolean takeaway;
+    private boolean extraCheese;
+    private boolean extraTopping;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        takeaway = false;
+        extraCheese = false;
+        extraTopping = false;
+        if(isVeg){
+            this.price += 300;
+            this.bill = "Base Price Of The Pizza: 300 \n";
+        }else{
+            this.price += 400;
+            this.bill = "Base Price Of The Pizza: 400 \n";
+        }
+
     }
 
     public int getPrice(){
@@ -17,18 +30,50 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheese){
+            extraCheese = true;
+            this.price += 80;
+        }
+
+
     }
 
-    public void addExtraToppings(){
+    public void addExtraToppings() {
         // your code goes here
+        if (!extraTopping){
+            extraTopping = true;
+            if (isVeg) {
+                this.price += 70;
+            } else {
+                this.price += 120;
+            }
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!takeaway) {
+            takeaway = true;
+            this.price += 20;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(extraCheese){
+            this.bill = bill + "Extra Cheese Added: 80 \n";
+        }
+        if(extraTopping){
+            if(isVeg){
+                this.bill = bill + "Extra Toppings Added: 70 \n";
+            }else{
+                this.bill = bill + "Extra Toppings Added: 120 \n";
+            }
+        }
+        if(takeaway){
+            this.bill = bill + "Paperbag Added: 20 \n";
+        }
+        this.bill = bill + "Total Price: "+ getPrice() +"\n";
         return this.bill;
     }
 }
